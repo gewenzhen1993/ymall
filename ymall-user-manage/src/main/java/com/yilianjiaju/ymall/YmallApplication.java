050c38e8aab51d2624414f0a233e9f8f5b196edd
gwz <15333535687gg@sina.com>
2019-12-16 16:43:42 +0800

    mapper扫描

--- ymall-user-manage/src/main/java/com/yilianjiaju/ymall/YmallApplication.java
+++ ymall-user-manage/src/main/java/com/yilianjiaju/ymall/YmallApplication.java
@@ -2,8 +2,10 @@ package com.yilianjiaju.ymall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.yilianjiaju.ymall.mapper")
 public class YmallApplication {
     public static void main(String[] args) {
         SpringApplication.run(YmallApplication.class,args);
